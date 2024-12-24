cb7ae0902f30383f5166bad28401591bb6a2ca63
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-12-24 11:00:25 +0700

    Algorithm skill checkpoint

--- question-3.js
+++ question-3.js
@@ -1,20 +1,23 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
-
+O(n) (Linear time) because The array of student is not sorted by ID,
+and using a for loop through all the values in the array.
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
-
-
+O(log n) (Logarithmic time) because The array of products is sorted by name,
+and the binary search will loop with a time complexity of log2n.
+in this case, the array has 3 key-value pairs so the count of looping is 2
 */
 
 /* 
 Which function is more efficient and why?
 Answer:
-
-
+Function findProductPrice is more efficient because Binary search does not loop through
+all the values in the array but it eliminate the values that not matched and continue looping,
+that make Binary search have time complexity lower than Linear search.
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,7 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
+O(n^2) (Quadratic time) because this function uses bubble sort to sort the values in the array by nested loop
 
 
 */
